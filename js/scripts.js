d3453c0266241e424b187514c7a50a9d9ca2f404
Morgan <morgan.lutz@gmail.com>
2015-09-18 14:22:40 -0700

    return correct line if number of bottles === 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,14 @@
 
 var playBottlesSong = function(numberOfBottles) {
-  if(numberOfBottles === 1) {
-    return "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall."
-  } else {
-    return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
-  }
+  debugger;
+    if(numberOfBottles === 2) {
+      return "2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall."
+    }
+    else if(numberOfBottles === 1) {
+      return "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall."
+    } else {
+      return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+    }
 };
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -3,8 +3,12 @@ describe('playBottlesSong', function() {
     expect(playBottlesSong(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
 
-  it("Returns correct line of song if number of bottles is only one", function() {
+  it("Returns correct line of song if number of bottles is one", function() {
     expect(playBottlesSong(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall.");
+  });
+
+  it("Returns correct line of song if number of bottles is two", function() {
+    expect(playBottlesSong(2)).to.equal("2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall.");
   });
 
 
