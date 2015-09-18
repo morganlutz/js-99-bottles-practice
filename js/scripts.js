8d21ed2b18199a77de81342f941c7962094f5550
Morgan <morgan.lutz@gmail.com>
2015-09-18 14:49:33 -0700

    returns all lines of song using recursion

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 
 var playBottlesSong = function(numberOfBottles) {
   debugger;
-
   if(numberOfBottles > 2) {
-    return (numberOfBottles + " bottles of beer on the wall, " + numberOfBottles + " bottles of beer. Take one down and pass it around, " + (numberOfBottles -1) + " bottles of beer on the wall.");
-    } else if(numberOfBottles === 2) {
-      return "2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall."
-    } else if(numberOfBottles === 1) {
-      return "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall."
+    return (numberOfBottles + " bottles of beer on the wall, "
+    + numberOfBottles + " bottles of beer. Take one down and pass it around, "
+    + (numberOfBottles -1) + " bottles of beer on the wall.")
+    + playBottlesSong(numberOfBottles - 1);
+    } if(numberOfBottles === 2) {
+      return "2 bottles of beer on the wall, 2 bottles of beer."
+      + " Take one down and pass it around, 1 bottle of beer on the wall."
+      + playBottlesSong(numberOfBottles - 1);
+    } if(numberOfBottles === 1) {
+      return "1 bottle of beer on the wall, 1 bottle of beer."
+      + " Take one down and pass it around, 0 bottles of beer on the wall."
+      + playBottlesSong(numberOfBottles - 1);
     } else {
       return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
     }
--- spec/specs.js
+++ spec/specs.js
@@ -15,5 +15,12 @@ describe('playBottlesSong', function() {
     expect(playBottlesSong(3)).to.equal("3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall.");
   });
 
+  it("Returns all lines of song in descending order", function() {
+    expect(playBottlesSong(3)).to.equal("3 bottles of beer on the wall, 3 bottles of beer. Take one down and pass it around, 2 bottles of beer on the wall." +
+  "\n2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall." +
+  "\n1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall." +
+  "\nNo more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+  });
+
 
 });
