e785ba6d8d2d574f4c099c6cabef3d5d60fd9361
Morgan <morgan.lutz@gmail.com>
2015-09-18 14:18:38 -0700

    return correct line if number of bottles === 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 
 var playBottlesSong = function(numberOfBottles) {
-debugger;
-  return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+  if(numberOfBottles === 1) {
+    return "1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall."
+  } else {
+    return "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+  }
 };
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -3,5 +3,9 @@ describe('playBottlesSong', function() {
     expect(playBottlesSong(0)).to.equal("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
   });
 
+  it("Returns correct line of song if number of bottles is only one", function() {
+    expect(playBottlesSong(1)).to.equal("1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, 0 bottles of beer on the wall.");
+  });
+
 
 });
